baa118ff1be91dd58ed21f0c3ef2465ef82d24ea
dheerajpille <dheerajpille@gmail.com>
2017-10-17 12:36:52 -0400

    Implemented changing focus on EditText after button click

--- app/src/main/java/com/dheeraj/pille/minimalist/MainActivity.java
+++ app/src/main/java/com/dheeraj/pille/minimalist/MainActivity.java
@@ -1,9 +1,12 @@
 package com.dheeraj.pille.minimalist;
 
+import android.app.Activity;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutCompat;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -23,17 +26,36 @@ public class MainActivity extends AppCompatActivity {
         taskEditText = (EditText)findViewById(R.id.taskEditText);
         taskAddButton = (Button)findViewById(R.id.taskAddButton);
 
+        taskEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if (b) {
+                    Toast.makeText(getBaseContext(), "Focused", Toast.LENGTH_SHORT).show();
+                    taskAddButton.setVisibility(View.VISIBLE);
+                    taskAddButton.setEnabled(true);
+                } else {
+                    Toast.makeText(getBaseContext(), "Not focused", Toast.LENGTH_SHORT).show();
+                    taskAddButton.setVisibility(View.GONE);
+                    taskAddButton.setEnabled(false);
+                }
+            }
+        });
 
         // Sets a button listener for clicks
         taskAddButton.setOnClickListener(new View.OnClickListener() {
 
             // Processes when button is clicked
             public void onClick(View v) {
+
                 // Get string value from EditText
                 taskString = taskEditText.getText().toString();
 
+                // Clears EditText field and focus
+                taskEditText.getText().clear();
+                taskEditText.clearFocus();
+
                 if (taskString.isEmpty()) {
-                    Toast.makeText(getBaseContext(), "Yello", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getBaseContext(), "Empty", Toast.LENGTH_SHORT).show();
                 } else {
                     Toast.makeText(getBaseContext(), taskString, Toast.LENGTH_SHORT).show();
                 }
