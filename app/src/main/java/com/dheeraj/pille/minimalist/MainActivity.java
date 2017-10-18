6ada189c7b6c03f124c4a69ac962eed0e773a22e
dheerajpille <dheerajpille@gmail.com>
2017-10-18 09:36:36 -0400

    Added done input on keyboard to enter task in EditText

--- app/src/main/java/com/dheeraj/pille/minimalist/MainActivity.java
+++ app/src/main/java/com/dheeraj/pille/minimalist/MainActivity.java
@@ -1,22 +1,27 @@
 package com.dheeraj.pille.minimalist;
 
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText taskEditText;
-    private ImageButton taskAddButton;
     private String taskString;
 
-    private Animation zoom_in, zoom_out, slide_left;
+    // Remove this
+    private ImageButton taskAddButton;
+
+    private Animation zoom_in, zoom_out;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +31,6 @@ public class MainActivity extends AppCompatActivity {
 
         zoom_in = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.zoom_in);
         zoom_out = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.zoom_out);
-        slide_left = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_left);
 
         taskEditText = (EditText)findViewById(R.id.taskEditText);
         taskAddButton = (ImageButton)findViewById(R.id.taskAddButton);
@@ -45,17 +49,47 @@ public class MainActivity extends AppCompatActivity {
                 // Sets button visibility/enabled status accordingly
                 if (b) {
                     taskEditText.startAnimation(zoom_in);
+
+                    // Adds elevation to create shadow on EditText
                     taskEditText.setElevation(50);
+
+                    // TODO: remove these
                     taskAddButton.setVisibility(View.VISIBLE);
                     taskAddButton.setEnabled(true);
                 } else {
                     // Hint that appears when EditText is not in focus
                     taskEditText.setHint("Add task");
+
+                    taskEditText.startAnimation(zoom_out);
+
+                    // Removes shadow from EditText
                     taskEditText.setElevation(0);
+
+                    // TODO: remove these
                     taskAddButton.setVisibility(View.GONE);
                     taskAddButton.setEnabled(false);
-                    taskEditText.startAnimation(zoom_out);
                 }
+            }
+        });
+
+        taskEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if (i == EditorInfo.IME_ACTION_DONE) {
+                    taskString = taskEditText.getText().toString();
+
+                    // Clears EditText field and focus
+                    taskEditText.getText().clear();
+                    taskEditText.clearFocus();
+
+                    if (!taskString.isEmpty()) {
+                        Toast.makeText(getBaseContext(), taskString, Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                }
+
+                Toast.makeText(getBaseContext(), "Empty", Toast.LENGTH_SHORT).show();
+                return false;
             }
         });
 
