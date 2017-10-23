c604850ec788823b02d3a2f6f887076c31dc6b41
dheerajpille <dheerajpille@gmail.com>
2017-10-23 11:51:17 -0400

    Implemented saving tasks that are checked

--- app/src/main/java/com/dheeraj/pille/minimalist/RecyclerAdapter.java
+++ app/src/main/java/com/dheeraj/pille/minimalist/RecyclerAdapter.java
@@ -1,15 +1,21 @@
 package com.dheeraj.pille.minimalist;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+
 import java.util.ArrayList;
 
 public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+
+    // SAVE_KEY used to store and retrieve task data from SharedPreferences
+    public static final String SAVE_KEY = "save_key";
 
     private Context context;
     private ArrayList<Task> taskArrayList;
@@ -46,6 +52,7 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
 
             // Icon made by Google from www.flaticon.com
             holder.taskText.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_check_box_black_24dp, 0, 0, 0);
+
         } else {
 
             // Icon made by Google from www.flaticon.com
@@ -56,6 +63,15 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
 
             @Override
             public void onClick(View view) {
+
+                // SharedPreferences to store and retrieve data
+                SharedPreferences pref = context.getSharedPreferences(SAVE_KEY, context.MODE_PRIVATE);
+
+                // SharedPreferences editor to store data
+                SharedPreferences.Editor editor =  pref.edit();
+
+                // Gson to store and retrieve data for SharedPreferences
+                Gson gson = new Gson();
 
                 task.toggleChecked();
 
@@ -78,6 +94,17 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
                     notifyItemInserted(0);
 
                 }
+
+                // Converts tasks as JSON
+                String json = gson.toJson(taskArrayList);
+
+                // Removes old JSON
+                editor.remove(SAVE_KEY).commit();
+
+                // Inserts new JSON
+                editor.putString(SAVE_KEY, json);
+                editor.commit();
+
             }
         });
     }
