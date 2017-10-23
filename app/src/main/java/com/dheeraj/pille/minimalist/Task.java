8ad8b8e5a6d7158b453ea57805195f6a62f22d93
dheerajpille <dheerajpille@gmail.com>
2017-10-23 14:59:42 -0400

    Added comments for Task

--- app/src/main/java/com/dheeraj/pille/minimalist/Task.java
+++ app/src/main/java/com/dheeraj/pille/minimalist/Task.java
@@ -1,22 +1,28 @@
 package com.dheeraj.pille.minimalist;
 
 public class Task {
+
+    // Task-defining variables
     String text;
     boolean checked;
 
+    // Task constructor
     public Task(String text, boolean checked) {
         this.text = text;
         this.checked = checked;
     }
 
+    // Gets text value
     public String getText() {
         return this.text;
     }
 
+    // Gets checked value
     public boolean getChecked() {
         return this.checked;
     }
 
+    // Toggles checked value
     public void toggleChecked() {
         this.checked = !this.checked;
     }
